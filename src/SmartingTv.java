87127817dd3ca1ec482bcb64c13d3eeb0f88e771
kauan <kauan.aguilamartinez@gmail.com>
2026-03-09 19:47:46 -0300

    primeiro commit

---
+++ src/SmartingTv.java
@@ -0,0 +1,98 @@
+import java.lang.classfile.constantpool.StringEntry;
+
+public class SmartingTv{
+    private String marca, modelo;
+    private int volume;
+    private boolean internet;
+
+    public SmartingTv(){
+    this.marca = " ";
+    this.modelo = " ";
+
+    }
+    public SmartingTv(String marca, String modelo, int volume ){
+        this.setMarca(marca);
+        this.setModelo(modelo);
+        this.setVolume(volume);
+        this.internet = false;
+    }
+    //setters e getters
+    public void setVolume(int volume) {
+        if (volume >= 0 && volume <= 100) {
+            this.volume = volume;
+
+        } else {
+            System.out.println("Volume invalido");
+        }
+    }
+        public int getVolume(){
+            return this.volume;
+        }
+        public void setMarca(String marca){
+        //a marca precisa ser maiuscula
+            char aux = marca.charAt(0);
+            boolean resp = Character.isUpperCase(aux);
+            if(resp){
+                this.marca = marca;
+        }
+        else{
+            System.out.println("Marca invalida");
+            }
+        }
+        public void setModelo(String modelo){
+            if (modelo.length() <= 30) {
+
+                this.modelo = modelo;
+            } else{
+                System.out.println("modelo invalido");
+            }
+        }
+        public  String getModelo(){
+        return this.modelo;
+        }
+        public void openYT(){
+        if (this.conectinternet()){
+            System.out.println("Abrindo youtube");
+        }
+        else{
+            System.out.println("Sem internet para abrir o youtube");
+        }
+        }
+        private boolean conectinternet(){
+        System.out.println("Buscando sinal de wifi");
+        System.out.println("Verificando credenciais");
+        System.out.println("Autenticando");
+        int random = (int) (Math.random() *10);
+        if (random < 5){
+            this.internet =  true;
+            return true;
+        }
+        else{
+            this.internet = false;
+            return false;
+        }
+        }
+        public void aumentaVolume(int X){
+        if (X > 0){
+            this.volume += X;
+            System.out.println("Volume +" +volume);
+        }
+        else {
+            System.out.println("Não da para abaixar o volume a baixo de 0 ");
+        }
+        }
+        public void diminuirVolume(int x){
+        if(x > 0){
+            this.volume -= x;
+            System.out.println("Volume -" +volume);
+        }
+        else System.out.println("Não é possivel diminuir o volume se o numero for positivo");
+        }
+
+    public String toString(){
+        return "Marca " + this.marca + " Modelo " + this.modelo + " Volume " + this.volume +
+                " Conectado na internet" +(this.internet ? " Sim" : " Não");
+    }
+
+
+}
---
+++ src/TestaTv.java
@@ -0,0 +1,24 @@
+public class TestaTv {
+
+    public static void main(String[] args){
+        SmartingTv obj1 = new SmartingTv();
+
+        obj1.setVolume (120);
+        obj1.setVolume (30);
+        obj1.openYT();
+
+
+        System.out.println("Volume " + obj1.getVolume());
+        SmartingTv objt2 = new SmartingTv("Sansung", "QL2 32", -30);
+        System.out.println(objt2.toString());
+        objt2.openYT();
+        objt2.aumentaVolume(5);
+        System.out.println(objt2.toString());
+        objt2.diminuirVolume(3);
+        System.out.println(objt2.toString());
+
+
+    }
+}
+
+

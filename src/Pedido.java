edad767b794721b859c533882119966cb464f3df
kauan <kauan.aguilamartinez@gmail.com>
2026-03-10 20:42:51 -0300

    Adicionando o Array para o pedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,18 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 public class Pedido {
     private int id;
     private Date data;
     private Cliente cliente;
+    private ArrayList<ItemProduto> itemProduto;
 
     public Pedido() {
+        this.itemProduto = new ArrayList<>(); //aloca espaço na memória para vetor
     }
 
     public Pedido(int id, Date data, Cliente cliente) {
         this.id = id;
         this.data = data;
         this.cliente = cliente;
+        this.itemProduto = new ArrayList<>();
     }
 
+    public void adicionaItemProduto(int id, int qtde, Produto produto){
+        ItemProduto aux = new ItemProduto(id,qtde,produto);
+        this.itemProduto.add(aux);
+    }
     public int getId() {
         return id;
     }
@@ -42,7 +51,7 @@ public class Pedido {
                 "id=" + id +
                 ", data=" + data +
                 ", cliente=\n   " + cliente +
-                '}';
+                '}' + "iten" + itemProduto;
     }
 }
 

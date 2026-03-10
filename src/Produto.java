e58e0700d10c11c19985ba32611374dab3b78a22
kauan <kauan.aguilamartinez@gmail.com>
2026-03-10 20:00:53 -0300

    Criando Produto

---
+++ src/Produto.java
@@ -0,0 +1,57 @@
+public class Produto {
+    private int id;
+    private String nome, descricao;
+    private float preco;
+
+    public Produto() {
+    }
+
+    public Produto(int id, String nome, String descricao, float preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}

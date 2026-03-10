abb4259a7ec4ba6a9c13b28bdff03fa4bcfee951
kauan <kauan.aguilamartinez@gmail.com>
2026-03-10 20:04:05 -0300

    Criando o Item produto

---
+++ src/ItemProduto.java
@@ -0,0 +1,47 @@
+public class ItemProduto {
+    private int id;
+    private int qtde;
+    private Produto produto;
+
+    public ItemProduto() {
+    }
+
+    public ItemProduto(int id, int qtde, Produto produto) {
+        this.id = id;
+        this.qtde = qtde;
+        this.produto = produto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getQtde() {
+        return qtde;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setQtde(int qtde) {
+        this.qtde = qtde;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemProduto{" +
+                "id=" + id +
+                ", qtde=" + qtde +
+                ", produto=" + produto +
+                '}';
+    }
+}

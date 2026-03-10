1a17616a4cad3979508af21ed6b1e4de087d9cdb
kauan <kauan.aguilamartinez@gmail.com>
2026-03-10 20:43:54 -0300

    Fazendo um Pedido

--- src/TestaTodoParte.java
+++ src/TestaTodoParte.java
@@ -4,9 +4,12 @@ public class TestaTodoParte   {
     public static void main(String[] args){
             //cliente é idependente do pedido
         Cliente cl1 = new Cliente(110, "Marquitos", "40028922", "Rua Beijamim Arrola");
+
         Pedido ped1 = new Pedido(1, new Date(), cl1);
+
         Produto prod1 = new Produto(55,"Mouse", "Acessório para computador", 55);
-        ItemProduto ip1 = new ItemProduto(11, 3, prod1);
+
+        ped1.adicionaItemProduto(55, 1, prod1);
         System.out.println(ped1.toString());
 
     }

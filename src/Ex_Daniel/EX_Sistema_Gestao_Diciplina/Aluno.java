a45a49735b55fb03a3b284785218887ee7799670
Kauan <kauan.aguilamartinez@gmail.com>
2026-03-14 15:48:27 -0300

    Criando o projeto para o exercicio sistema gestao de dicipliana e implementando a class Aluno

---
+++ src/Ex_Daniel/EX_Sistema_Gestao_Diciplina/Aluno.java
@@ -0,0 +1,61 @@
+package EX_Sistema_Gestao_Diciplina;
+public class Aluno{
+
+    //atributos
+    public int id;
+    public String nome;
+    public int ra;
+    public String curso;
+
+    public Aluno() {
+    }
+
+    public Aluno(int id, String nome, int ra, String curso) {
+        this.id = id;
+        this.nome = nome;
+        this.ra = ra;
+        this.curso = curso;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getRa() {
+        return ra;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setRa(int ra) {
+        this.ra = ra;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Aluno{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", ra=" + ra +
+                ", curso='" + curso + '\'' +
+                '}';
+    }
+}

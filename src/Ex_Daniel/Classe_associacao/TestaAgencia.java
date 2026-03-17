e0a9e03b6f1ee645b535e74c52fe4da0d323d44f
kauan <kauan.aguilamartinez@gmail.com>
2026-03-17 20:22:21 -0300

    Implementando a class TestaAgencia

---
+++ src/Ex_Daniel/Classe_associacao/TestaAgencia.java
@@ -0,0 +1,14 @@
+package Ex_Daniel.Classe_associacao;
+
+import java.time.LocalDateTime;
+
+public class TestaAgencia {
+    public static void main(String[] args){
+        Passageiro pas1 = new Passageiro("123", "fulano");
+        System.out.println(pas1.toString());
+
+        LocalDateTime data = LocalDateTime.of(2026, 10, 14, 10, 15);
+        Voo vo1 = new Voo(999, "commeta", "Campinas", "Salvador", data);
+        System.out.println(vo1.toString());
+    }
+}

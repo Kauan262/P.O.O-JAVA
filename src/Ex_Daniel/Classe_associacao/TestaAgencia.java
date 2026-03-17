37773dbcbef355548bde9c3a04b7e742f6e9b463
kauan <kauan.aguilamartinez@gmail.com>
2026-03-17 20:34:42 -0300

    Criando a class reserva e implementando ela a class Testa Agencia

--- src/Ex_Daniel/Classe_associacao/TestaAgencia.java
+++ src/Ex_Daniel/Classe_associacao/TestaAgencia.java
@@ -10,5 +10,8 @@ public class TestaAgencia {
         LocalDateTime data = LocalDateTime.of(2026, 10, 14, 10, 15);
         Voo vo1 = new Voo(999, "commeta", "Campinas", "Salvador", data);
         System.out.println(vo1.toString());
+
+        Reserva r1 = new Reserva(55, LocalDateTime.now(), 12, pas1, vo1);
+        System.out.println(r1.toString());
     }
 }
